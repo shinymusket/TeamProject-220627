786cd2957d36b9cde2758fd2e40ec173c2abce0a
suyeon <j1m3p2@naver.com>
2022-06-29 15:05:33 +0900

    member2

--- WebContent/nav/nav.jsp
+++ WebContent/nav/nav.jsp
@@ -4,7 +4,7 @@
 	<ul>
 		<li><a href="">후보조회</a></li>
 		<li><a href="">투표하기</a></li>
-		<li><a href="">투표검수조회</a></li>
+		<li><a href="VS?command=vote_List">투표검수조회</a></li>
 		<li><a href="">후보자등수</a></li>
 		<li><a href="VS?command=home">홈으로</a></li>
 	</ul>
--- src/com/green/vote/controller/ActionFactory.java
+++ src/com/green/vote/controller/ActionFactory.java
@@ -2,6 +2,7 @@ package com.green.vote.controller;
 
 import com.green.vote.controller.action.Action;
 import com.green.vote.controller.action.IndexAction;
+import com.green.vote.controller.action.VoteListAction;
 
 public class ActionFactory {
 	private ActionFactory() {}
@@ -15,7 +16,12 @@ public class ActionFactory {
 		
 		if (command.equals("home")) {
 			action = new IndexAction();
+		} 
+		else if (command.equals("vote_List")) {  // 홈 -> 투표검수조회 페이지 이동 클래스
+			action = new VoteListAction();
 		}
+		
+		
 		
 		return action;
 	}
