ff9e204ac76ba68caf4fc18d507af1d7b8fb2ff9
shinymusket <grace211340@gmail.com>
2022-06-30 14:51:46 +0900

    add insertVote

--- src/com/green/vote/dao/VoteDAO.java
+++ src/com/green/vote/dao/VoteDAO.java
@@ -2,6 +2,7 @@ package com.green.vote.dao;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
@@ -210,7 +211,35 @@ public class VoteDAO {
 		
 		return sex;
 	}
-	
+
+
+
+	public void insertVote(VoteVO vVo) { // 투표하기
+		String sql = "INSERT INTO TBL_VOTE_202005 VALUES(?, ?, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement psmt = null;
+
+		try {
+		conn = DBManager.getConnection();
+		psmt  = conn.prepareStatement(sql);
+
+		psmt.setString(1, vVo.getV_jumin());
+		psmt.setString(2, vVo.getV_name());
+		psmt.setString(3, vVo.getM_no());
+		psmt.setString(4, vVo.getV_time());
+		psmt.setString(5, vVo.getV_area());
+		psmt.setString(6, vVo.getV_confirm());
+
+		psmt.executeUpdate();
+
+
+		} catch(Exception e) {
+
+		} finally {
+		DBManager.close(conn, psmt);
+		}
+
+		}
 	
 	
 	
